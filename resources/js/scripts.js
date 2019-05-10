1fbd2b4fab38cc9685e67d971bd0ac9cd36c7aa3
Anirudha Bose <ani07nov@gmail.com>
2019-05-10 16:49:00 +0530

    Do a website makeover

---
+++ resources/js/scripts.js
@@ -0,0 +1,53 @@
+/**
+ * 1. Menu Mobile
+ * 2. Preload
+ */
+
+'use strict';
+
+(function ($) {
+	$(document).ready(function () {
+
+		var $header = $('.header')
+
+		// 1. Menu Mobile
+		var $btnMenu = $('.menu-mobile'),
+			$hideMenu = $('.hide-menu');
+
+		$btnMenu.on('click', function () {
+			$header.toggleClass('active');
+
+			if ($header.hasClass('active')) {
+				$hideMenu.addClass('active');
+			}
+			else {
+				$hideMenu.removeClass('active');
+			}
+		});
+		$hideMenu.on('click', function () {
+			$header.removeClass('active');
+			$hideMenu.removeClass('active');
+		});
+
+		$('.menu-item-has-children', '.main-menu').on('click', ' > a', function (e) {
+			var ww = $(window).width();
+
+			if (ww <= 991) {
+				var $parent = $(e.target).closest('.menu-item-has-children');
+				e.preventDefault();
+				$('>.sub-menu', $parent).slideToggle(400);
+			}
+		});
+
+		// 2. Preload
+
+		var $preload = $('#preload');
+
+		if ($preload.length) {
+			$(window).on('load', function () {
+				$preload.fadeOut(400);
+			});
+		}
+	});
+
+})(jQuery);
