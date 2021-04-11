fba566f4fe37d59ef8a17ba7549059bee6baa2ec
Anirudha Bose <anirudha.bose@ledger.fr>
2021-04-11 16:31:13 +0530

    Refactor extern to make it less repetitive

--- src/components/extern.js
+++ src/components/extern.js
@@ -6,55 +6,62 @@ const Tag = styled.span`
   font-size: smaller;
 `
 
+const items = [
+  {
+    date: "Apr 10, 2021",
+    tags: ["series"],
+    title: "#30DaysOfBitcoin",
+    href: "https://twitter.com/onybose/status/1380719582516715525",
+  },
+  {
+    date: "Mar 24, 2021",
+    tags: ["talk"],
+    title: "DevBreak – Let's talk elliptic curves and bitcoin",
+    href:
+      "https://www.devbreak.io/workshop/lets-talk-elliptic-curves-and-bitcoin",
+  },
+  {
+    date: "Jan 09, 2020",
+    tags: ["workshop"],
+    title: "Paris P2P Festival – Demystifying the cryptography behind bitcoin",
+    href: "https://onyb.gitbook.io/secp256k1-python",
+  },
+  {
+    date: "Sep 20, 2019",
+    tags: ["talk"],
+    title: "ESoWC Day @ ECMWF – ecPoint-Calibrate",
+    href:
+      "https://docs.google.com/presentation/d/1NcAI7ZV6s0VCdKMS6SdXfGcSH5pHPhlH9drzJx0x4YU/edit?usp=sharing",
+  },
+  {
+    date: "Feb 20, 2018",
+    tags: ["talk"],
+    title: "Django Paris – reobject",
+    href: "http://slides.com/onyb/reobject",
+  },
+]
+
 const Extern = () => (
   <>
     <h5>Extern</h5>
     <ul>
-      <li>
-        <div className="post-date code">
-          <small>Mar 24, 2021</small>
-        </div>
-        <div className="title">
-          <Tag className="code">[talk]</Tag>{" "}
-          <a href="https://www.devbreak.io/workshop/lets-talk-elliptic-curves-and-bitcoin">
-            DevBreak – Let's talk elliptic curves and bitcoin
-          </a>
-        </div>
-      </li>
-
-      <li>
-        <div className="post-date code">
-          <small>Jan 09, 2020</small>
-        </div>
-        <div className="title">
-          <Tag className="code">[workshop]</Tag>{" "}
-          <a href="https://onyb.gitbook.io/secp256k1-python">
-            Paris P2P Festival – Demystifying the cryptography behind bitcoin
-          </a>
-        </div>
-      </li>
-
-      <li>
-        <div className="post-date code">
-          <small>Sep 20, 2019</small>
-        </div>
-        <div className="title">
-          <Tag className="code">[talk]</Tag>{" "}
-          <a href="https://docs.google.com/presentation/d/1NcAI7ZV6s0VCdKMS6SdXfGcSH5pHPhlH9drzJx0x4YU/edit?usp=sharing">
-            ESoWC Day @ ECMWF – ecPoint-Calibrate
-          </a>
-        </div>
-      </li>
-
-      <li>
-        <div className="post-date code">
-          <small>Feb 20, 2018</small>
-        </div>
-        <div className="title">
-          <Tag className="code">[talk]</Tag>{" "}
-          <a href="http://slides.com/onyb/reobject">Django Paris – reobject</a>
-        </div>
-      </li>
+      {items.map((item, idx) => (
+        <li key={idx}>
+          <div className="post-date code">
+            <small>{item.date}</small>
+          </div>
+          <div className="title">
+            {item.tags.map((tag, idx) => (
+              <>
+                <Tag key={idx} className="code">
+                  [{tag}]
+                </Tag>{" "}
+              </>
+            ))}
+            <a href={item.href}>{item.title}</a>
+          </div>
+        </li>
+      ))}
     </ul>
   </>
 )
