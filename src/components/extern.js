85d47022be0be1f0e106f5a7e52bc6f557086a39
Anirudha Bose <anirudha.bose@alumni.cern>
2021-08-24 11:20:48 +0530

    Small updates to about text and dates

--- src/components/extern.js
+++ src/components/extern.js
@@ -8,39 +8,39 @@ const Tag = styled.span`
 
 const items = [
   {
-    date: "July, 2021",
+    date: "Jul 2021",
     tags: ["talk"],
     title: "Browsers3000 – A Brave New Wallet",
     href: "https://blog.ipfs.io/brave-new-wallet",
   },
   {
-    date: "Apr 10, 2021",
+    date: "Apr 2021",
     tags: ["series"],
     title: "#30DaysOfBitcoin",
     href: "https://twitter.com/onybose/status/1380719582516715525",
   },
   {
-    date: "Mar 24, 2021",
+    date: "Mar 2021",
     tags: ["talk"],
     title: "DevBreak – Let's talk elliptic curves and bitcoin",
     href:
       "https://www.devbreak.io/workshop/lets-talk-elliptic-curves-and-bitcoin",
   },
   {
-    date: "Jan 09, 2020",
+    date: "Jan 2020",
     tags: ["workshop"],
     title: "Paris P2P Festival – Demystifying the cryptography behind bitcoin",
     href: "https://onyb.gitbook.io/secp256k1-python",
   },
   {
-    date: "Sep 20, 2019",
+    date: "Sep 2019",
     tags: ["talk"],
     title: "ESoWC Day @ ECMWF – ecPoint-Calibrate",
     href:
       "https://docs.google.com/presentation/d/1NcAI7ZV6s0VCdKMS6SdXfGcSH5pHPhlH9drzJx0x4YU/edit?usp=sharing",
   },
   {
-    date: "Feb 20, 2018",
+    date: "Feb 2018",
     tags: ["talk"],
     title: "Django Paris – reobject",
     href: "http://slides.com/onyb/reobject",
