95f7e0a5835ffda843714510b01d84fd98bfc0f8
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2021-10-31 22:37:22 +0530

    feat: Add basic components and basic design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
+import Content from "./Components/Content"
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
+      <Content></Content>
     </div>
   );
 }
